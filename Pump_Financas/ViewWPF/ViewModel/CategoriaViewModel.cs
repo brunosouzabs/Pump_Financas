53936e48444428bb93e8488a405e63dba86ea2ae
brunosouzabs <bsbdso@gmail.com>
2018-09-20 21:53:36 -0300

    CategoriaViewModel  Concluida

--- Pump_Financas/ViewWPF/ViewModel/CategoriaViewModel.cs
+++ Pump_Financas/ViewWPF/ViewModel/CategoriaViewModel.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ViewWPF.ViewModel
 {
-    class CategoriaViewModel
+    class CategoriaViewModel    
     {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        #region CategoriaViewModel 
+        private int categoriaID;
+        public int CategoriaID
+        {
+            get { return categoriaID; }
+            set
+            {
+                categoriaID = value;
+                NotifyPropertyChanged("CategoriaID");
+            }
+        }
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                NotifyPropertyChanged("Nome");
+            }
+        }
+        private bool status;
+        public bool Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+                NotifyPropertyChanged("Status");
+            }
+        }        
+        #endregion
     }
 }
