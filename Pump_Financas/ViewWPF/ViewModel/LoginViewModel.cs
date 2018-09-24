c531de4a0be0d14bb02fcfac480201547fd84119
brunosouzabs <bsbdso@gmail.com>
2018-09-24 20:00:35 -0300

    LoginViewModel construída

--- Pump_Financas/ViewWPF/ViewModel/LoginViewModel.cs
+++ Pump_Financas/ViewWPF/ViewModel/LoginViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,35 @@ namespace ViewWPF.ViewModel
 {
     class LoginViewModel
     {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        #region LoginViewModel
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+        private string senha;
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                senha = value;
+                NotifyPropertyChanged("Senha");
+            }
+        }
+        #endregion
     }
 }
